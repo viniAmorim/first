2652e3bbed934a193f66f72f146fde9749a169ed
vini <amorimm.viniciuss@gmail.com>
2020-10-07 14:39:46 -0300

    Estilos globais

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,9 +4,8 @@ import { Title } from './styles';
 
 function Main() {
   return (
-    <Title error={false}>
-      Main
-      <small>menor</small>
+    <Title>
+      Hello World
     </Title>
   );
 }
